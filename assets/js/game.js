92df716cc76c755048805cf016111ecbfa4e75b4
Erin M <erin.michon@gmail.com>
2021-10-20 15:26:41 -0400

    high-score feature progress made

--- assets/js/game.js
+++ assets/js/game.js
@@ -150,13 +150,23 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
+  console.log("I made it here");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-  } else {
-    window.alert("You've lost your robot in battle!");
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
   }
+  console.log(highScore);
+  console.log(playerInfo.money);
+
+  //if player has more money than the high score, player has new high score
+  if (playerInfo.money > highScore ) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
